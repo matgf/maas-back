3ca08ae557ca00bf1a205c1be32e2a4704ae3056
Matias Gaete <matiasgf18@gmail.com>
2022-11-13 18:34:39 -0300

    feat: added engineer type to service resolver

--- app/graphql/types/engineer_type.rb
+++ app/graphql/types/engineer_type.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module Types
-  class ServiceType < Types::BaseObject
+  class EngineerType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String
+    field :first_name, String
+    field :last_name, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :shifts, [Types::ShiftType]
-    field :engineers, [Types::EngineerType]
+    field :service_id, Integer
   end
 end
