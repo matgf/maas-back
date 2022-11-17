74abaabce064f3eb3ce64cfdbac43e88fdc8141b
Matias Gaete <matiasgf18@gmail.com>
2022-11-16 23:47:42 -0300

    tests: added tests for shift_engineer

--- spec/factories/shift_engineers.rb
+++ spec/factories/shift_engineers.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :shift_engineer do
-    
+    shift_id { FactoryBot.create(:shift).id }
+    engineer_id { FactoryBot.create(:engineer).id }
   end
 end
--- spec/models/shift_engineer_spec.rb
+++ spec/models/shift_engineer_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe ShiftEngineer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before(:each) do
+      @shift_engineer = FactoryBot.create(:shift_engineer)
+    end
+    it 'has a valid factory' do
+      expect(@shift_engineer).to be_valid
+    end
+  end
+
+  describe 'belongs_to engineer' do
+    context 'when shift_engineer has engineer' do
+      it 'allows accessing engineer from the engineer' do
+        engineer = FactoryBot.create(:engineer)
+        shift_engineer = FactoryBot.create(:shift_engineer, engineer_id: engineer.id)
+        expect(shift_engineer.engineer.id).to be(engineer.id)
+      end
+    end
+  end
+
+  describe 'belongs_to shift' do
+    context 'when shift_engineer has shift' do
+      it 'allows accessing engineer from the shift' do
+        shift = FactoryBot.create(:shift)
+        shift_engineer = FactoryBot.create(:shift_engineer, shift_id: shift.id)
+        expect(shift_engineer.shift.id).to be(shift.id)
+      end
+    end
+  end
 end
