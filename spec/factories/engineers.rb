746b3b417c18dfed361144044fe880e3d0e1e2fa
Matias Gaete <matiasgf18@gmail.com>
2022-11-16 23:12:13 -0300

    feat: added factory and tests for engineer

--- spec/factories/engineers.rb
+++ spec/factories/engineers.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :engineer do
-    first_name { "MyString" }
-    last_name { "MyString" }
+    first_name { Faker::Artist.name }
+    last_name { Faker::Food.fruits }
+    service_id { FactoryBot.create(:service).id }
   end
 end
--- spec/models/engineer_spec.rb
+++ spec/models/engineer_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Engineer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before(:each) do
+      @engineer = FactoryBot.create(:engineer)
+    end
+    it 'has a valid factory' do
+      expect(@engineer).to be_valid
+    end
+
+    it 'validates presence of first name' do
+      expect(@engineer).to validate_presence_of(:first_name)
+    end
+
+    it 'validates presence of last name' do
+      expect(@engineer).to validate_presence_of(:last_name)
+    end
+
+    it 'validates presence of service id' do
+      expect(@engineer).to validate_presence_of(:service_id)
+    end
+  end
 end
