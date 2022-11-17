3724119ccfb79bb6854ee9a8ad38d3fe79258ed4
Matias Gaete <matiasgf18@gmail.com>
2022-11-16 23:29:24 -0300

    tests: added has many test for service

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -5,12 +5,35 @@ RSpec.describe Service, type: :model do
     before(:each) do
       @service = FactoryBot.create(:service)
     end
+
     it 'has a valid factory' do
       expect(@service).to be_valid
     end
 
     it 'validates presence of first name' do
       expect(@service).to validate_presence_of(:name)
+    end
+  end
+
+  describe 'has_many engineers' do
+    context 'when service has engineers' do
+      it 'allows accessing engineers from the rule' do
+        service = FactoryBot.create(:service)
+        engineers = FactoryBot.create_list(:engineer, 2, service_id: service.id)
+
+        expect(service.engineers).to match_array(engineers)
+      end
+    end
+  end
+
+  describe 'has_many shifts' do
+    context 'when service has engineers' do
+      it 'allows accessing engineers from the rule' do
+        service = FactoryBot.create(:service)
+        shifts = FactoryBot.create_list(:shift, 2, service_id: service.id)
+
+        expect(service.shifts).to match_array(shifts)
+      end
     end
   end
 end
