fd7e6b76d53c374a1f8c4364b4586f7eee1678ce
Matias Gaete <matiasgf18@gmail.com>
2022-11-16 23:11:51 -0300

    feat: added validations for Engineer

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -3,4 +3,7 @@
 class Engineer < ApplicationRecord
   belongs_to :service
   has_many :shift_engineers, dependent: :destroy
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :service_id, presence: true
 end
