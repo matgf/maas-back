619e2d444a3510de93057da8e42a53685e20f659
Matias Gaete <matiasgf18@gmail.com>
2022-11-17 00:23:54 -0300

    feat: added validations for shift

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,4 +3,9 @@
 class Shift < ApplicationRecord
   belongs_to :service
   has_many :shift_engineers
+  validates :service_id, presence: true
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
 end
