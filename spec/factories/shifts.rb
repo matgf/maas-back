e285f52272b3b6b6845f327cd8309b24c628a2fa
Matias Gaete <matiasgf18@gmail.com>
2022-11-17 00:24:26 -0300

    tests: added shift model tests

--- spec/factories/shifts.rb
+++ spec/factories/shifts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :shift do
-    
+    date { Faker::Date.between(from: 7.weeks.ago, to: Date.today)}
+    assigned { false }
+    start_time { DateTime.now }
+    end_time { DateTime.now + 1.hours }
+    engineer_id { FactoryBot.create(:engineer).id }
+    service_id { FactoryBot.create(:service).id }
   end
 end
--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Shift, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before(:each) do
+      @shift = FactoryBot.create(:shift)
+    end
+    it 'has a valid factory' do
+      expect(@shift).to be_valid
+    end
+
+    it 'validates presence of service' do
+      expect(@shift).to validate_presence_of(:service_id)
+    end
+
+    it 'validates presence of date' do
+      expect(@shift).to validate_presence_of(:date)
+    end
+
+    it 'validates presence of start_time' do
+      expect(@shift).to validate_presence_of(:start_time)
+    end
+
+    it 'validates presence of end_time' do
+      expect(@shift).to validate_presence_of(:end_time)
+    end
+  end
+
+  describe 'belongs_to service' do
+    context 'when shift has service' do
+      it 'allows accessing service from the shift' do
+        service = FactoryBot.create(:service)
+        shift = FactoryBot.create(:shift, service_id: service.id)
+        expect(shift.service.id).to be(service.id)
+      end
+    end
+  end
+  describe 'has_many shift_engineers' do
+    context 'when shift has shift_engineers' do
+      it 'allows accessing shift_engineers from the shift' do
+        shift = FactoryBot.create(:shift)
+        shift_engineers = FactoryBot.create_list(:shift_engineer, 2, shift_id: shift.id)
+
+        expect(shift.shift_engineers).to match_array(shift_engineers)
+      end
+    end
+  end
 end
